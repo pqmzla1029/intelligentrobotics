65253e1159fbfbe0d80ec458dcdea406e7c68882
pqmzla1029 <igormonteiro101@gmail.com>
2019-11-04 20:49:58 -0500

    1st official commit

---
+++ install1.sh
@@ -0,0 +1,10 @@
+python3 -V
+apt-get install -y python3-pip
+apt-get install build-essential libssl-dev libffi-dev python3-dev cmake unzip pkg-config
+apt-get install libjpeg-dev libpng-dev libtiff-dev
+apt-get install libjasper-dev
+apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+apt-get install libxvidcore-dev libx264-dev
+apt-get install libgtk-3-dev
+apt-get install libatlas-base-dev gfortran
+apt-get install -y python3-venv
---
+++ install2.sh
@@ -0,0 +1,8 @@
+
+pip install numpy
+pip install scipy
+pip install scikit-learn
+pip install opencv-python
+pip install opencv-contrib-python
+pip install tensorflow
+pip install tensorflow-gpu
